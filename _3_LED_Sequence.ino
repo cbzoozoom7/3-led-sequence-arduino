0a6d3b530abd2437ef291beaa9d3d3bb5165623e
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-02-14 20:47:32 -0600

    Add files via upload

---
+++ _3_LED_Sequence.ino
@@ -0,0 +1,39 @@
+int delayTime = 500;
+int led0Pin = 0;
+int led1Pin = 1;
+int led2Pin = 2;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led0Pin,OUTPUT);
+pinMode(led1Pin,OUTPUT);
+pinMode(led2Pin,OUTPUT);
+for(int blinkCount = 0; blinkCount < 3; blinkCount++) {
+digitalWrite(led0Pin,HIGH);
+digitalWrite(led1Pin,HIGH);
+digitalWrite(led2Pin,HIGH);
+delay(delayTime);
+digitalWrite(led0Pin,LOW);
+digitalWrite(led1Pin,LOW);
+digitalWrite(led2Pin,LOW);
+delay(delayTime);
+}
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led0Pin,HIGH);
+digitalWrite(led1Pin,LOW);
+digitalWrite(led2Pin,LOW);
+delay(delayTime);
+digitalWrite(led0Pin,HIGH);
+digitalWrite(led1Pin,HIGH);
+digitalWrite(led2Pin,LOW);
+delay(delayTime);
+digitalWrite(led0Pin,HIGH);
+digitalWrite(led1Pin,HIGH);
+digitalWrite(led2Pin,HIGH);
+delay(delayTime);
+digitalWrite(led0Pin,LOW);
+digitalWrite(led1Pin,LOW);
+digitalWrite(led2Pin,LOW);
+delay(delayTime);
+}
